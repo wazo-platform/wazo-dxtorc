4964bffca1cee26791f8ae19c2cd8a6cdd6781ac
Francois Blackburn <fblackburn@avencall.com>
2015-03-03 14:53:04 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='xivo-dxtorc',
+    version='0.1',
+    description='XIVO dxtora client',
+    maintainer='Avencall',
+    maintainer_email='xivo-users@lists.proformatique.com',
+    url='http://www.xivo.io/',
+    license='GPLv3',
+    scripts=['bin/dxtorc'],
+)
