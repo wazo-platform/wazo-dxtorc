aae3f2292d4f7ca8c3f72f538dd40ec183e9024c
Sebastien Duthil <sduthil@wazo.community>
2017-04-28 19:09:15 -0400

    replace distutils with setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='xivo-dxtorc',
-    version='0.1',
+    version='0.2',
     description='XIVO dxtora client',
     author='Wazo Authors',
     author_email='dev@wazo.community',
